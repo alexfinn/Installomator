f29a170f43af95c63e218e371df999167a3318a0
alexfinn <91119429+alexfinn@users.noreply.github.com>
2024-05-31 15:37:10 +0100

    Create gitcredmanager.sh

---
+++ fragments/labels/gitcredmanager.sh
@@ -0,0 +1,10 @@
+gitcredentialmanager)
+    name="gcm-osx-arm64-[0-9.]*"
+    type="pkg"
+    packageID="gcmcore
+com.microsoft.gitcredentialmanager"
+    downloadURL="$(downloadURLFromGit git-ecosystem git-credential-manager)"
+    appNewVersion="$(versionFromGit git-ecosystem git-credential-manager)"
+    expectedTeamID="VEKTX9H2N7"
+    ;;
+
